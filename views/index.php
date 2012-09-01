8ea635471863123efd2fdfabce6a05300658bd18
Carlos Gajardo <cgajardo@litmon.com>
2012-09-01 17:36:15 -0400

    Baseline

---
+++ views/index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <!-- Bootstrap -->
+    <link href="views/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="views/js/bootstrap.min.js"></script>
+  </body>
+</html>
